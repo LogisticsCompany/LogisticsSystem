3b909758f8a7252ecf8acd5f5f377b84aea3816a
dell-pc <565267339@qq.com>
2018-09-07 15:28:57 +0800

    init

--- src/main/java/com/example/logistics_system/controller/AdminController.java
+++ src/main/java/com/example/logistics_system/controller/AdminController.java
@@ -1,8 +1,26 @@
 package com.example.logistics_system.controller;
 
+import com.example.logistics_system.service.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class AdminController
 {
+    @Autowired
+    private AdminService adminService;
+
+    @RequestMapping(value = "/adminLogin", method = RequestMethod.POST)
+    public String login(String username, String password, HttpServletRequest request)
+    {
+        if (adminService.loginService(username, password))
+            request.getSession().setAttribute("username", username);
+        else
+            request.getSession().setAttribute("loginError", 0);
+        return "";
+    }
 }
--- src/main/java/com/example/logistics_system/dao/AdminDAO.java
+++ src/main/java/com/example/logistics_system/dao/AdminDAO.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AdminDAO extends JpaRepository<Admin, Integer>
 {
+    Admin findByUsername(String username);
 }
